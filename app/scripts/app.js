612d2a76b927766ecbac056620fca297e876573e
machouahi <machouahi@hotmail.com>
2017-05-11 17:23:43 +0200

    start page with yeoman

---
+++ app/scripts/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name ffExpensesApp
+ * @description
+ * # ffExpensesApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('ffExpensesApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ffExpensesApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ffExpensesApp
+ */
+angular.module('ffExpensesApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });

cf838dc7e2e300dbb17759357a53c1e2dfa12d01
machouahi <machouahi@hotmail.com>
2017-05-17 23:41:40 +0200

    javascript styleguide code

--- app/scripts/services/transactionResource.service.js
+++ app/scripts/services/transactionResource.service.js
@@ -3,20 +3,20 @@
 
   function TransactionResourceService ($resource, constants, $http) {
     var transactions = $resource(constants.URL.getTransactions, {}, {
-        query: {method: 'GET',
-          isArray: true,
-          transformResponse: $http.defaults.transformResponse.concat([
-            function (data) {
-              angular.forEach(data, function (transaction) {
-                transaction.formattedDate = getFormattedTransactionDate(transaction)
-              })
+      query: {method: 'GET',
+        isArray: true,
+        transformResponse: $http.defaults.transformResponse.concat([
+          function (data) {
+            angular.forEach(data, function (transaction) {
+              transaction.formattedDate = getFormattedTransactionDate(transaction)
+            })
 
-              return data
-            }
-          ])}
-      }),
-      saldo = $resource(constants.URL.getSaldo, {}, {
-        query: {method: 'GET', isArray: true}})
+            return data
+          }
+        ])}
+    })
+    var saldo = $resource(constants.URL.getSaldo, {}, {
+      query: {method: 'GET', isArray: true}})
 
     function getFormattedTransactionDate (transaction) {
       return transaction.date.dayOfMonth + '-' + transaction.date.monthValue + '-' + transaction.date.year
